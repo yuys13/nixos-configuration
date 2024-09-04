d4777ee056278f098aae036e6aaf994c899c7967
yuys13 <yuys13@users.noreply.github.com>
2024-09-04 23:31:19 +0900

    Use fish as default shell

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -59,7 +59,9 @@
     description = "yuys13";
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [];
+    shell = pkgs.fish;
   };
+  programs.fish.enable = true;
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
