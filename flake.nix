b7ab573259ba91b7ae23294d0aeb07e5a7e0ab12
yuys13 <yuys13@users.noreply.github.com>
2026-03-22 17:26:26 +0900

    feat(flake): introduce flake-parts for modular configuration

--- flake.nix
+++ flake.nix
@@ -10,59 +10,82 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    # Flake parts
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
   };
 
   outputs =
-    {
+    inputs@{
       self,
       nixpkgs,
       home-manager,
+      flake-parts,
       ...
-    }@inputs:
-    let
-      inherit (self) outputs;
-    in
-    {
-      # NixOS configuration entrypoint
-      # Available through 'nixos-rebuild --flake .#your-hostname'
-      nixosConfigurations = {
-        # FIXME replace with your hostname
-        hyper-nixos = nixpkgs.lib.nixosSystem {
-          specialArgs = {
-            inherit inputs outputs;
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
+
+      flake = {
+        # NixOS configuration entrypoint
+        # Available through 'nixos-rebuild --flake .#your-hostname'
+        nixosConfigurations = {
+          # FIXME replace with your hostname
+          hyper-nixos = nixpkgs.lib.nixosSystem {
+            specialArgs = {
+              inherit inputs;
+              outputs = self.outputs;
+            };
+            # > Our main nixos configuration file <
+            modules = [
+              ./nixos/configuration.nix
+
+              home-manager.nixosModules.home-manager
+              {
+                home-manager.useGlobalPkgs = true;
+                home-manager.useUserPackages = true;
+
+                home-manager.users.yuys13 = {
+                  imports = [
+                    ./home-manager/sway.nix
+                    ./home-manager/home.nix
+                  ];
+                };
+              }
+            ];
           };
-          # > Our main nixos configuration file <
-          modules = [
-            ./nixos/configuration.nix
+        };
 
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-
-              home-manager.users.yuys13 = {
-                imports = [
-                  ./home-manager/sway.nix
-                  ./home-manager/home.nix
-                ];
-              };
-            }
-          ];
+        # Standalone home-manager configuration entrypoint
+        # Available through 'home-manager --flake .#your-username@your-hostname'
+        homeConfigurations = {
+          # FIXME replace with your username@hostname
+          "yuys13" = home-manager.lib.homeManagerConfiguration {
+            pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+            extraSpecialArgs = {
+              inherit inputs;
+              outputs = self.outputs;
+            };
+            # > Our main home-manager configuration file <
+            modules = [ ./home-manager/home.nix ];
+          };
         };
       };
 
-      # Standalone home-manager configuration entrypoint
-      # Available through 'home-manager --flake .#your-username@your-hostname'
-      homeConfigurations = {
-        # FIXME replace with your username@hostname
-        "yuys13" = home-manager.lib.homeManagerConfiguration {
-          pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-          extraSpecialArgs = {
-            inherit inputs outputs;
-          };
-          # > Our main home-manager configuration file <
-          modules = [ ./home-manager/home.nix ];
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
+        {
+          # ... your perSystem configuration here ...
         };
-      };
     };
 }
