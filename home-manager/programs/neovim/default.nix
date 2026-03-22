81187dfd38ed85eda92b10d9503238dda5bca558
yuys13 <yuys13@users.noreply.github.com>
2026-03-22 12:20:34 +0900

    refactor(neovim): stop using programs.neovim and manage nvim manually
    - Install neovim and extra packages via home.packages.
    - Set EDITOR and VISUAL in home.sessionVariables.
    - Add vi.fish function via xdg.configFile.

--- home-manager/programs/neovim/default.nix
+++ home-manager/programs/neovim/default.nix
@@ -3,19 +3,25 @@ let
   sources = pkgs.callPackage ../../../_sources/generated.nix { };
 in
 {
-  programs.neovim = {
-    enable = true;
-    defaultEditor = true;
-    viAlias = true;
-    extraPackages = with pkgs; [
-      neovim-remote
-      gcc
-      gitlint
-      tree-sitter
-    ];
+  home.packages = with pkgs; [
+    neovim
+    neovim-remote
+    gcc
+    gitlint
+    tree-sitter
+  ];
+
+  home.sessionVariables = {
+    EDITOR = "nvim";
+    VISUAL = "nvim";
   };
+
   xdg.configFile."nvim" = {
     source = sources.dotfiles.src + "/home/XDG_CONFIG_HOME/nvim";
     recursive = true;
+  };
+
+  xdg.configFile."fish/functions/vi.fish" = {
+    source = sources.dotfiles.src + "/home/XDG_CONFIG_HOME/fish/functions/vi.fish";
   };
 }
