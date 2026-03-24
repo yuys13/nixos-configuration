466e5b3437629ca7302d0254c7ff2ffed45b2daf
yuys13 <yuys13@users.noreply.github.com>
2026-03-24 22:21:41 +0900

    feat(nixos): optimize and clean up nix settings

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -34,16 +34,22 @@
     ];
   };
 
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
-
-  nix.optimise.automatic = true;
-  nix.gc = {
-    automatic = true;
-    dates = "weekly";
-    options = "--delete-older-than 30d";
+  nix = {
+    channel.enable = false;
+    settings = {
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+      auto-optimise-store = pkgs.stdenv.hostPlatform.isLinux;
+      warn-dirty = false;
+    };
+    optimise.automatic = true;
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 30d";
+    };
   };
 
   # Bootloader.
