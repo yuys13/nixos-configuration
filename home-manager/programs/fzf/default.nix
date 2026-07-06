001b2391fdec850f5348bae1b1e2d72e9f8d9077
yuys13 <yuys13@users.noreply.github.com>
2026-07-06 19:06:11 +0900

    home-manager/fzf: group options into nested attribute sets and fix deprecation warnings

--- home-manager/programs/fzf/default.nix
+++ home-manager/programs/fzf/default.nix
@@ -6,11 +6,15 @@
       "--layout reverse"
       "--height 40%"
     ];
-    changeDirWidgetCommand = "fd -t d";
-    changeDirWidgetOptions = [ "--preview 'eza --icons --tree --level=1 --color=always {}'" ];
-    fileWidgetCommand = "fd -t f -L -H -E .git";
-    fileWidgetOptions = [
-      "--preview 'bat --color=always --style=header,grid --line-range :100 {}'"
-    ];
+    changeDirWidget = {
+      command = "fd -t d";
+      options = [ "--preview 'eza --icons --tree --level=1 --color=always {}'" ];
+    };
+    fileWidget = {
+      command = "fd -t f -L -H -E .git";
+      options = [
+        "--preview 'bat --color=always --style=header,grid --line-range :100 {}'"
+      ];
+    };
   };
 }
